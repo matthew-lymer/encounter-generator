6fa32efb0f6a05f608a200e9a0be7eb9f252a9f0
matthew-lymer <31863428+matthew-lymer@users.noreply.github.com>
2020-10-31 17:01:38 +0000

    fix spell icon

--- js/generator.js
+++ js/generator.js
@@ -788,7 +788,7 @@ $(document).ready(function(){
         selection += '<a class="reRollSpell reroll" style="display:none" href="#"><h4>SEARCH</h4></a>';
 
         var tempHTML =  '<div class="box overflow large">' +
-                        '    <h2 class="handle"><img src="images/monster.png" width="25" height="25" /> Spell</h2>' +
+                        '    <h2 class="handle"><img src="images/spell.png" width="25" height="25" /> Spell</h2>' +
                         '    <div class="inner">' +
                         '        <div class="text-center top-controls">' +
                                     selection +
